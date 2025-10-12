56111a766343e43630dbc8d62e21880d089bb2b7
Wade Ashby <wade.ashby@hotmail.com>
2025-10-12 14:28:13 -0500

    Added Assignment4

---
+++ Assignment4/Fraction.java
@@ -0,0 +1,31 @@
+/**
+*
+* Using your previous Fraction class, add the following methods to the class
+*  -Fraction add(Fraction f)
+*  -Fraction subtract(Fraction f)
+*  -Fraction multiply(Fraction f)
+*  -Fraction divide(Fraction f)
+*  -int compareTo(Fraction f)
+*
+**/
+public class Fraction{
+   private int numerator;
+   private int denominator;
+   
+   public Fraction(int n, int d){
+      //TODO: Update this constructor from previous assignments to also throw
+      //      IllegalArgumentException with the message "Cannot have 0 in denominator."
+   }
+   
+   public Fraction(int n){
+      //TODO: Finish this constructor so that n is over 1 for a whole number
+   }
+   
+   /*
+     TODO: create the required method so that when 
+     a Fraction object is part of a print() then output
+     is numerator/denominator.
+   */
+   //public...
+      
+}
---
+++ Assignment4/FractionDriver.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class FractionDriver{
+   public static void main(String[] args) throws FileNotFoundException{
+      //TODO: Create a method that will test the add(), subtract(), multiply(), and divide() methods in your Fraction class  
+      //      Also test the constructor method when passing 0 in to the denominator. NOTE: Your driver should not crash because
+      //      you handle the exception properly.
+   }
+}
